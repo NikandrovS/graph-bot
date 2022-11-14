d61ad407786956cce41dc8da9be5994dd7bebddb
NikandrovS <nikandrovse@gmail.com>
2022-11-14 11:54:47 +0300

    Translations + subscription_period limit

--- src/bot/handlers/generateCoinPriceMessage.js
+++ src/bot/handlers/generateCoinPriceMessage.js
@@ -2,35 +2,40 @@ import sendNotifications from "./sendNotifications.js";
 import translations from "../translations.js";
 
 export default async (boardId, boardUrl, oldPrice, newPrice) => {
-  const priceDiff = ((newPrice - oldPrice) / 100000000000000000).toFixed(4);
+  const priceDiff = ((newPrice - oldPrice) / 1000000000000000000).toFixed(3);
   const oldAmountInCoins = oldPrice / 100000000000000000;
   const newAmountInCoins = newPrice / 100000000000000000;
   const percent = ((newAmountInCoins / oldAmountInCoins - 1) * 100).toFixed(1);
   const tokensLockedBefore = Math.pow(oldAmountInCoins, 2) * 5;
   const tokensLockedAfter = Math.pow(newAmountInCoins, 2) * 5;
-  const tokensSpent = Math.abs(((tokensLockedAfter - tokensLockedBefore) / 90).toFixed(1));
-
   const increase = priceDiff > 0;
+
+  const delimiter = increase ? 90 : 100;
+  const tokensSpent = Math.abs(((tokensLockedAfter - tokensLockedBefore) / delimiter).toFixed(1));
 
   const ru =
     `⚜️ <a href="https://main.community/b/${boardUrl}">b/${boardUrl}</a> ${percent}%\n` +
-    `${increase ? "Покупка: " : "Продажа: "}` +
+    `${increase ? translations.ru.buyEvent : translations.ru.sellEvent}: ` +
     priceDiff +
     " BC\n" +
-    `${increase ? "Потрачено: " : "Получено: "}` +
+    `${increase ? translations.ru.spentEvent : translations.ru.recievedEvent}: ` +
     tokensSpent +
     " MAIN\n" +
-    `Стоимость: ${oldAmountInCoins.toFixed(1)} -> ${newAmountInCoins.toFixed(1)}`;
+    translations.ru.priceChange
+      .replace("<%= oldPrice %>", oldAmountInCoins.toFixed(1))
+      .replace("<%= newPrice %>", newAmountInCoins.toFixed(1));
 
   const en =
     `⚜️ <a href="https://main.community/b/${boardUrl}">b/${boardUrl}</a> ${percent}%\n` +
-    `${increase ? "Покупка: " : "Продажа: "}` +
+    `${increase ? translations.en.buyEvent : translations.en.sellEvent}: ` +
     priceDiff +
     " BC\n" +
-    `${increase ? "Потрачено: " : "Получено: "}` +
+    `${increase ? translations.en.spentEvent : translations.en.recievedEvent}: ` +
     tokensSpent +
     " MAIN\n" +
-    `Стоимость: ${oldAmountInCoins.toFixed(1)} -> ${newAmountInCoins.toFixed(1)}`;
+    translations.en.priceChange
+      .replace("<%= oldPrice %>", oldAmountInCoins.toFixed(1))
+      .replace("<%= newPrice %>", newAmountInCoins.toFixed(1));
 
   sendNotifications({ ru, en }, "coin-price-change", boardId);
 };
