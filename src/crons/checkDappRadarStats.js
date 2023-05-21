054cfd6c8769cd83cb0b442f65755f05cfdf9fb3
NikandrovS <nikandrovse@gmail.com>
2023-05-21 22:39:14 +0600

    fixooo

--- src/crons/checkDappRadarStats.js
+++ src/crons/checkDappRadarStats.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default () => {
-  new CronJob("* * * *", fetchDappRadar, null, true, "Europe/Moscow");
+  new CronJob("0 0 * * * *", fetchDappRadar, null, true, "Europe/Moscow");
 };
 
 export const fetchDappRadar = async () => {
