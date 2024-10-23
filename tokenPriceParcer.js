53e0b909fc582b2b062198f613193dfbc6978748
NikandrovS <nikandrovse@gmail.com>
2024-10-23 19:07:34 +0200

    cmc price parcing

--- tokenPriceParcer.js
+++ tokenPriceParcer.js
@@ -1,9 +1,9 @@
 // import fetchPrice from "./token-price-parser/fetchMoralisApi.js";
-// import fetchPrice from "./token-price-parser/fetchCMCApi.js";
-import fetchPrice from "./token-price-parser/fetchLlamaApi.js";
+import fetchPrice from "./token-price-parser/fetchCMCApi.js";
+// import fetchPrice from "./token-price-parser/fetchLlamaApi.js";
 import { CronJob } from "cron";
 
 (() => {
   // new CronJob("*/5 * * * *", fetchPrice, null, true, "Europe/Moscow");
-  new CronJob("*/15 * * * * *", fetchPrice, null, true, "Europe/Moscow");
+  new CronJob("0 * * * * *", fetchPrice, null, true, "Europe/Moscow");
 })();
